f58575559bf31ffc7c8f51fa962c778caaba633e
Nishkarsh-Jain <94288727+Nishkarsh-Jain@users.noreply.github.com>
2022-12-10 10:46:55 +0530

    setup angular.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HomeComponent, ProductsComponent, ContactComponent, AboutComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
