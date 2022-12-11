e434da57e34f3bfb9e53f78b9916ccd834a19bb4
Nishkarsh-Jain <94288727+Nishkarsh-Jain@users.noreply.github.com>
2022-12-11 10:25:34 +0530

    1 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 import { Navbar1Component } from './navbar1/navbar1.component';
+import { SideNavbar1Component } from './side-navbar1/side-navbar1.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, Navbar1Component],
+  declarations: [AppComponent, Navbar1Component, SideNavbar1Component],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/side-navbar1/side-navbar1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-navbar1',
+  templateUrl: './side-navbar1.component.html',
+  styleUrls: ['./side-navbar1.component.css']
+})
+export class SideNavbar1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
