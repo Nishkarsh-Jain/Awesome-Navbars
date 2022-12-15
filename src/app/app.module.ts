19565dd21856d282d9f9e98a5dfc84ebc3434f4b
Nishkarsh-Jain <94288727+Nishkarsh-Jain@users.noreply.github.com>
2022-12-15 11:41:05 +0530

    navbar2 development

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 
 import { Navbar1Component } from './navbar1/navbar1.component';
 import { SideNavbar1Component } from './side-navbar1/side-navbar1.component';
+import { SideNavbar2Component } from './side-navbar2/side-navbar2.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, Navbar1Component, SideNavbar1Component],
+  declarations: [AppComponent, Navbar1Component, SideNavbar1Component, SideNavbar2Component],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/side-navbar2/side-navbar2.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-navbar2',
+  templateUrl: './side-navbar2.component.html',
+  styleUrls: ['./side-navbar2.component.css'],
+})
+export class SideNavbar2Component implements OnInit {
+  bColor: boolean = false;
+  toggleSidebar: boolean = false;
+  mode: string = 'Dark Mode';
+  modeStatus: boolean = false;
+  constructor() {}
+
+  ngOnInit() {}
+
+  onToggle() {
+    this.toggleSidebar = !this.toggleSidebar;
+  }
+
+  onModeSwitch() {
+    this.bColor = !this.bColor;
+    if (this.bColor) {
+      this.mode = 'Light Mode';
+    } else {
+      this.mode = 'Dark Mode';
+    }
+  }
+}
