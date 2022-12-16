36c1ad4f947b0e9a83c1f39bb80bd36953c24bf4
Nishkarsh-Jain <94288727+Nishkarsh-Jain@users.noreply.github.com>
2022-12-16 18:15:11 +0530

    1 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { Navbar1Component } from './navbar1/navbar1.component';
 import { SideNavbar1Component } from './side-navbar1/side-navbar1.component';
 import { SideNavbar2Component } from './side-navbar2/side-navbar2.component';
 import { DemoTestComponent } from './demo-test/demo-test.component';
-import { FlexboxNavbarComponent } from './flexbox-navbar/flexbox-navbar.component';
+import { Navbar2Component } from './navbar2/navbar2.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
@@ -17,7 +17,7 @@ import { FlexboxNavbarComponent } from './flexbox-navbar/flexbox-navbar.componen
     Navbar1Component,
     SideNavbar1Component,
     SideNavbar2Component,
-    FlexboxNavbarComponent,
+    Navbar2Component,
     DemoTestComponent,
   ],
   bootstrap: [AppComponent],
