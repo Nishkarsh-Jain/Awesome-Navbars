ff110d93ca6ccc0408dbf5ab56f26e5352fe674c
Nishkarsh-Jain <94288727+Nishkarsh-Jain@users.noreply.github.com>
2022-12-16 11:25:03 +0530

    1 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { Navbar1Component } from './navbar1/navbar1.component';
 import { SideNavbar1Component } from './side-navbar1/side-navbar1.component';
 import { SideNavbar2Component } from './side-navbar2/side-navbar2.component';
 import { DemoTestComponent } from './demo-test/demo-test.component';
+import { FlexboxNavbarComponent } from './flexbox-navbar/flexbox-navbar.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
@@ -16,6 +17,7 @@ import { DemoTestComponent } from './demo-test/demo-test.component';
     Navbar1Component,
     SideNavbar1Component,
     SideNavbar2Component,
+    FlexboxNavbarComponent,
     DemoTestComponent,
   ],
   bootstrap: [AppComponent],
---
+++ src/app/flexbox-navbar/flexbox-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flexbox-navbar',
+  templateUrl: './flexbox-navbar.component.html',
+  styleUrls: ['./flexbox-navbar.component.css']
+})
+export class FlexboxNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
